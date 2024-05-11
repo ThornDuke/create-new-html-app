63623587187ddfe0f4d7b703f70c0ca094154845
Sergio Spina <sergio.spina@spindox.it>
2024-05-11 21:06:09 +0200

    App working

--- index.js
+++ index.js
@@ -6,35 +6,144 @@
  * licence: GPL-3.0-or-later
  */
 
-const fs = require('fs');
-const { name: programName, version: programVersion, description: programDescription } = require('./package.json');
+//const fs = require('fs');
+import fs from 'fs';
+import prompts from 'prompts';
+import {
+  htmlTemplate,
+  cssTemplate,
+  jsTemplate,
+  helpScreen,
+  prettierTemplate,
+  gitIgnoreTemplate,
+  launchTemplate,
+  changeLogTemplate,
+  readmeTemplate,
+} from './src/constants.js';
 
-const { htmlTemplate } = require('./src/constants');
+import metaData from './package.json' assert { type: 'json' };
 
 const [nodeLocation, fileLocation, projectName] = process.argv;
-const currentPath = process.cwd();
 
+switch (projectName) {
+  case undefined:
+  case '-h':
+  case '--help':
+    console.log(helpScreen(metaData.name, metaData.version, metaData.description));
+    process.exit();
+  case '-V':
+  case '--version':
+    console.log(`\n${metaData.name} v${metaData.version}\n`);
+    process.exit();
+  default:
+    break;
+}
+
+const currentPath = process.cwd();
+const EC_SUCCESS = 0;
+const EC_USER_TERMINATED = 1;
+
+// se la directory esiste già, chiede se sovrascriverla
+if (fs.existsSync(projectName)) {
+  (async () => {
+    const response = await prompts({
+      type: 'toggle',
+      name: 'answer',
+      message: `The '${projectName}' directory already exists. Do you want to overwrite it?`,
+      initial: true,
+      active: 'yes',
+      inactive: 'no',
+    });
+    if (response.answer === false) {
+      // se risposta è no...
+      process.exit(EC_USER_TERMINATED);
+    }
+  })();
+}
+
+// ... altrimenti:
+console.log('');
+console.log('');
+console.log(`##### ${metaData.name} v${metaData.version}`);
+console.log('###');
 // crea il sistema di directory
+console.log("### creo l'albero delle directory");
 try {
-  if (!fs.existsSync(projectName)) {
-    //---> forse è meglio "Overwrite?"
-    fs.mkdirSync(projectName);
-  }
-  if (!fs.existsSync(`${projectName}/HTML`)) {
-    fs.mkdirSync(`${projectName}/HTML`);
-  }
-  if (!fs.existsSync(`${projectName}/HTML/.vscode`)) {
-    fs.mkdirSync(`${projectName}/HTML/.vscode`);
-  }
-  if (!fs.existsSync(`${projectName}/HTML/public`)) {
-    fs.mkdirSync(`${projectName}/HTML/public`);
-  }
-  if (!fs.existsSync(`${projectName}/HTML/src`)) {
-    fs.mkdirSync(`${projectName}/HTML/src`);
-  }
+  fs.mkdirSync(projectName);
+  fs.mkdirSync(`${projectName}/HTML`);
+  fs.mkdirSync(`${projectName}/HTML/.vscode`);
+  fs.mkdirSync(`${projectName}/HTML/public`);
+  fs.mkdirSync(`${projectName}/HTML/src`);
 } catch (err) {
-  console.error(err);
+  console.error("Errore nella creazione dell'albero delle directory:", err);
 }
 
 // crea il file html
-fs.writeFileSync(`${projectName}/HTML/index.html`, htmlTemplate);
+console.log('### creo i file:');
+console.log('### - index.html');
+try {
+  fs.writeFileSync(`${projectName}/HTML/index.html`, htmlTemplate(projectName));
+} catch (err) {
+  console.error('Errore nella creazione del file HTML:', err);
+}
+
+// crea il file css
+console.log('### - public/styles.css');
+try {
+  fs.writeFileSync(`${projectName}/HTML/public/styles.css`, cssTemplate(projectName));
+} catch (err) {
+  console.error('Errore nella creazione del file css:', err);
+}
+
+// crea il file js
+console.log('### - public/script.js');
+try {
+  fs.writeFileSync(`${projectName}/HTML/public/script.js`, jsTemplate(projectName));
+} catch (err) {
+  console.error('Errore nella creazione del file js:', err);
+}
+
+// crea il file .prettierrc
+console.log('### - .prettierrc');
+try {
+  fs.writeFileSync(`${projectName}/HTML/.prettierrc`, prettierTemplate());
+} catch (err) {
+  console.error('Errore nella creazione del file .prettierrc:', err);
+}
+
+// crea il file .gitignore
+console.log('### - .gitignore');
+try {
+  fs.writeFileSync(`${projectName}/HTML/.gitignore`, gitIgnoreTemplate());
+} catch (err) {
+  console.error('Errore nella creazione del file .gitignore:', err);
+}
+
+// crea il file .vscode/launch.json
+console.log('### - .vscode/launch.json');
+try {
+  fs.writeFileSync(`${projectName}/HTML/.vscode/launch.json`, launchTemplate());
+} catch (err) {
+  console.error('Errore nella creazione del file launch.json:', err);
+}
+
+// crea il file CHANGELOG.md
+console.log('### - CHANGELOG.md');
+try {
+  fs.writeFileSync(`${projectName}/HTML/CHANGELOG.md`, changeLogTemplate());
+} catch (err) {
+  console.error('Errore nella creazione del file CHANGELOG.md:', err);
+}
+
+// crea il file README.md
+console.log('### - README.md');
+try {
+  fs.writeFileSync(`${projectName}/HTML/README.md`, readmeTemplate());
+} catch (err) {
+  console.error('Errore nella creazione del file README.md:', err);
+}
+
+console.log('###');
+console.log('### Progetto creato con successo');
+console.log('### Happy hacking!');
+console.log('###');
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
-export const htmlTemplate = `<!DOCTYPE html>
+export const htmlTemplate = appName => `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>${projectName}</title>
+    <title>${appName}</title>
     <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
@@ -12,3 +12,91 @@ export const htmlTemplate = `<!DOCTYPE html>
     <script src="script.js"></script>
   </body>
 </html>`;
+
+export const cssTemplate = appName => `/* project ${appName} */`;
+
+export const prettierTemplate = () => `{
+  "printWidth": 120,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "avoid",
+  "proseWrap": "always",
+  "htmlWhitespaceSensitivity": "css",
+  "embeddedLanguageFormatting": "auto",
+  "singleAttributePerLine": false
+}`;
+
+export const gitIgnoreTemplate = () => `.vscode`;
+
+export const launchTemplate = () => `{
+  // Usare IntelliSense per informazioni sui possibili attributi.
+  // Al passaggio del mouse vengono visualizzate le descrizioni degli attributi esistenti.
+  // Per altre informazioni, visitare: https://go.microsoft.com/fwlink/?linkid=830387
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "type": "chrome",
+      "request": "launch",
+      "name": "Open index.html",
+      "file": "./index.html"
+    }
+  ]
+}`;
+
+export const changeLogTemplate = () => `# Change Log
+
+<!--
+## [Unreleased] | [major.minor.patch] - yyyy-mm-dd
+### Added | Fixed | Changed | Removed | Deprecated | Security
+- filename {section}: description
+-->
+
+## [Unreleased]
+`;
+
+export const readmeTemplate = appName => `# ${appName}
+
+<!-- App description -->
+
+## Features
+
+## Requirements
+
+## Installation
+
+## How to use
+
+## Managing settings
+
+## Known Issues
+
+## Release Notes
+
+## Contributing
+
+## Acknowledgements
+`;
+
+export const jsTemplate = appName =>
+  `/**
+ * project ${appName}
+ */`;
+
+export const helpScreen = (appName, appVersion, appDescription) => `
+${appName} v${appVersion}
+
+${appDescription}
+
+Usage:
+
+${appName} <project-name>           create the project
+${appName} -h, --help               print this help and exit
+${appName} -V, --version            print the version number
+`;
